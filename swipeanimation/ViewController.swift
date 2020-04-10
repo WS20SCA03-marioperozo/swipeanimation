ba6c470e413b939e68353116c34e2726ed644edf
Mario <marioperozo@gmail.com>
2020-04-10 11:13:36 -0400

    Swipe Swipe

--- swipeanimation/ViewController.swift
+++ swipeanimation/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var picture: UIImageView!
+    var v: Float = 0
+    var audioPlayer: AVAudioPlayer = AVAudioPlayer();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        let path: String = Bundle.main.path(forResource: "Acid", ofType: "mp3")!;
+        print("path = \(path)");
 
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path));
+        } catch {
+            print(error)
+        }
+
+        audioPlayer.numberOfLoops = -1;   //infinite loop
+        audioPlayer.volume = 0.3;
+        audioPlayer.play();
+        
+        picture.image = UIImage(named: "frame_\(v)")
+        picture.alpha = 1.0;
+    }
+    
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        if v >= 0 && v <= 91 {
+            picture.image = UIImage(named: "frame_\(v)")
+            picture.image = UIImage(named: "frame_\(v+1)");
+            v = v + 1
+        }
+        else if v == 92 {
+                v = 0
+    }
+    
+        
+    }
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        if v >= 1 && v <= 92 {
+                picture.image = UIImage(named: "frame_\(v)")
+                picture.image = UIImage(named: "frame_\(v-1)");
+                v = v - 1
+            }
+            else if v == 0 {
+                    v = 92
+        }
+    }
+    
 
 }
-
